bbb123277342aaac70be43c45ff0a3f40ea30f96
Georges Racinet on purity.racinet.fr <georges@racinet.fr>
2011-03-12 22:47:20 +0100

    command to add devsetup to squeeze repo

--- cps-3.5-devsetup/bin/build.sh
+++ cps-3.5-devsetup/bin/build.sh
@@ -4,3 +4,4 @@ bin/make_deb.sh deb/squeeze
 cd ..
 bin/include_deb.sh opt-debian lenny cps-3.5-devsetup/packages/deb/common/current.deb
 bin/include_deb.sh opt-ubuntu karmic cps-3.5-devsetup/packages/deb/common/current.deb
+bin/include_deb.sh opt-debian squeeze cps-3.5-devsetup/packages/deb/squeeze/current.deb
