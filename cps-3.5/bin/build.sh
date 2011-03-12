199d352faf8c47f2cb3285dfe6d277b7481f2736
Georges Racinet on purity.racinet.fr <georges@racinet.fr>
2011-03-12 22:47:49 +0100

    Squeeze version of opt-zope-cps-3.5, fully bootstrapped

--- cps-3.5/bin/build.sh
+++ cps-3.5/bin/build.sh
@@ -4,7 +4,9 @@ bin/get_tarball.sh
 # debs that are common among distributions
 bin/clean.sh deb/common
 bin/make_deb.sh deb/common
+bin/make_deb.sh deb/squeeze
 cd ..
 
 bin/include_deb.sh opt-debian lenny cps-3.5/packages/deb/common/current.deb
 bin/include_deb.sh opt-ubuntu karmic cps-3.5/packages/deb/common/current.deb
+bin/include_deb.sh opt-debian squeeze cps-3.5/packages/deb/squeeze/current.deb
