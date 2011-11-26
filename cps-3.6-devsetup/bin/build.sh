709b48276e7f96e224369232caef037677d3086e
Georges Racinet on purity.racinet.fr <georges@racinet.fr>
2011-11-26 11:58:19 +0100

    Added opt-zope-cps-3.6-devsetup, done a month ago

---
+++ cps-3.6-devsetup/bin/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+bin/make_deb.sh deb/common
+bin/make_deb.sh deb/squeeze
+cd ..
+bin/include_deb.sh opt-debian lenny cps-3.5-devsetup/packages/deb/common/current.deb
+bin/include_deb.sh opt-ubuntu karmic cps-3.5-devsetup/packages/deb/common/current.deb
+bin/include_deb.sh opt-debian squeeze cps-3.5-devsetup/packages/deb/squeeze/current.deb
